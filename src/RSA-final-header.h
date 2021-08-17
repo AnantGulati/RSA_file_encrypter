54685c380967559d0351e0016d335e86e9d436cd
amrithasp02 <63729021+amrithasp02@users.noreply.github.com>
2021-08-17 10:52:33 +0530

    Final Program

---
+++ src/RSA-final-header.h
@@ -0,0 +1,8 @@
+int gcd(int , int );
+int FastExponention(int , int , int* , int* );
+int FindT(int a, int m, int n);
+int PrimarityTest(int a, int i);
+int inverse(int a, int b);
+int register_user(char* , char* ,char*);
+int read_file();
+int inbox(char* , char* , char* );
